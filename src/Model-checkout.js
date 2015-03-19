04e87f3bc389c88cf1a60a34a044fa39b19207bd
François Beuvens <francois.beuvens@gmail.com>
2015-03-19 14:42:58 +0100

    update sort

--- src/Model-checkout.js
+++ src/Model-checkout.js
@@ -5,8 +5,14 @@ module.exports = function(mongoose){
 	_(mongoose.Model).extend({
 
 		getRoot: function(options, callback){
+			var sort = options.req.query.sort;
+			if(!sort && options.req.query.sortBy) {
+				sort = {};
+				sort[options.req.query.sortBy] = options.req.query.sortHow || 'asc';
+			}
+			
 			this.find(options.req.query.filters)
-				.sort(options.req.query.sort)
+				.sort(sort)
 				.skip(options.req.query.offset || options.req.query.paginate && options.req.query.paginate.offset)
 				.limit(options.req.query.limit || options.req.query.paginate && options.req.query.paginate.limit)
 				.exec(callback);
